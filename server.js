8d99e4a913d258e6ddffd29237e50fa86b0682de
adibte <adibte@hotmail.com>
2020-02-21 02:16:22 +0330

    finalizing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path')
 
 // Database Connection
 connectDB();
@@ -9,14 +10,19 @@ connectDB();
 // Middlewares
 app.use(express.json({extended:false}))
 
-app.get('/',(req,res)=>{
-    res.json({msg:'hello world'})
-})
-
 // Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 const PORT =  process.env.PORT || 85;
 app.listen(PORT,()=>{
